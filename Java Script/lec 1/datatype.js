95636ca9bcfe6e07fa34d986bcbe35a8cde5614b
laxmidharvicky-tech <laxmidhar.vicky@gmail.com>
2026-07-17 16:44:12 +0530

    java script 1st lec

---
+++ Java Script/lec 1/datatype.js
@@ -0,0 +1,17 @@
+console.log(typeof 10)
+console.log(typeof 10.2)
+console.log(typeof NaN)
+
+console.log(typeof "10")
+console.log(typeof "jena")
+
+console.log(typeof true)
+console.log(typeof false)
+
+console.log(typeof undefined)
+
+console.log(typeof null)
+
+console.log(typeof Symbol(10))
+
+console.log(typeof BigInt(23          ))
---
+++ Java Script/lec 1/script.js
@@ -0,0 +1,4 @@
+console.log("hellooooooo world !")
+
+        let h1= document.querySelector("h1")
+        h1.style.color = 'red'
