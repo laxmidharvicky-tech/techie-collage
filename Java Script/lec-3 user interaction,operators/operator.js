b2cf12066acac11fe2b538cd02aade417051b49a
laxmidharvicky-tech <laxmidhar.vicky@gmail.com>
2026-07-21 08:58:57 +0530

    use interaction and operators

---
+++ Java Script/lec-3 user interaction,operators/operator.js
@@ -0,0 +1,15 @@
+// console.log(5 == "5")
+// console.log(5 === "5")
+
+console.log(2 << 3)
+
+console.log(8 << 5)//256
+console.log(4 << 3) //32
+                                  
+console.log(64 >> 5)//2
+console.log(256 >> 5)//8
+
+// >>> /(unsigned right shift)
+
+let a = 10,y = 20
+console.log(a > y ? a : y)
---
+++ Java Script/lec-3 user interaction,operators/userinter acttion.js
@@ -0,0 +1,21 @@
+// alert("hello")
+// window.alert("good morning")
+
+// let a = prompt("hiii")
+// let name =prompt("enter your name")
+// console.log(name)
+
+// if( name){
+    // console.log("your name is ",name)
+// }else{
+
+    // console.log("name is " ,prompt("re-enter username"))
+
+// console.log(a)
+
+// let choice = confirm("are you enjoy")
+// console.log(choice)
+
+console.log("hii")
+console.warn("hello")
+console.error("java")
