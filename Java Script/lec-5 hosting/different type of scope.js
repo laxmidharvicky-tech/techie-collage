6f5b88c6a49c4717c517f89aac91f8e88dd5d21b
laxmidharvicky-tech <laxmidhar.vicky@gmail.com>
2026-07-25 08:33:22 +0530

    different type of scope

---
+++ Java Script/lec-5 hosting/different type of scope.js
@@ -0,0 +1,14 @@
+var a = 10;
+console.log(a)
+
+function fun(){
+    console.log("hello");
+} 
+fun()
+
+let b = 10;
+console.log(b)
+let value = function(){
+    console.log("hello")
+}
+value()
---
+++ Java Script/lec-5 hosting/hosting.js
@@ -0,0 +1,15 @@
+// console.log(a)
+// fun()
+
+// var a = 10;
+//  function fun(){
+//     console.log("hello")
+//  }
+
+console.log(a)
+value(22)
+
+var a = 10;
+function value(num){
+    console.log(num);
+}
