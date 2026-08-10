a6341c3fec16d93c6e5d2e3eb47d9dea11008c7b
laxmidharvicky-tech <laxmidhar.vicky@gmail.com>
2026-08-11 02:58:54 +0530

    practice commit

---
+++ Java Script/lec 12 -String/practice siting.js
@@ -0,0 +1,64 @@
+// const a = " hello everyone "
+// const findsize = a.length
+// console.log(findsize)
+
+// const b = a.toUpperCase()
+// console.log(b)
+
+// const c = a.toLowerCase()
+// console.log(c)
+// const d = a.trim()
+// console.log(d)
+
+
+// const aa = a.trimStart()
+// console.log(aa)
+
+// const bb = a.trimEnd()
+// console.log(bb)
+
+// const cc = a.toString()
+// console.log(cc)
+
+// const b = "I Love Java"
+// const dd = b.charAt(2)
+// console.log(dd)
+
+// const cc = b.charCodeAt(4)
+// console.log(cc)
+
+// const c = "i like java"
+// const add = b.concat(" ",c)
+// console.log(add)
+
+// const dd = b.includes("Java")
+// console.log(dd)
+
+// const cc = b.indexOf("J")
+// console.log(cc)
+
+// const dd = b.lastIndexOf("a")
+// console.log(dd)
+
+// const cc = b.replace("J", "K")
+// console.log(cc)
+
+// const dd = b.replaceAll("a", "k")
+// console.log(dd)
+
+// const dd = b.repeat(3)
+// console.log(dd)
+
+const a = "i love java"
+
+// const dd = a.split(" ")
+// console.log(dd)
+
+// const dd = a.slice(3, 8)
+// console.log(dd)
+
+// const dd = a.substring(3, 9)
+// console.log(dd)
+
+const cc = a.endsWith("a")
+console.log(cc)
