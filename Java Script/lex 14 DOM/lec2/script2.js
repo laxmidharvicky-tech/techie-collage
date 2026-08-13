88ce13fee50fa637c99fc28c422a62c507055fee
laxmidharvicky-tech <laxmidhar.vicky@gmail.com>
2026-08-14 01:15:51 +0530

    dom

---
+++ Java Script/lex 14 DOM/lec2/script2.js
@@ -0,0 +1,4 @@
+const image = document.getElementsByTagName('img')[0]
+console.log('\\\\\\', image)
+console.log(image[0])
+image[0].src = 'data://static.vecteezy.com/system/resources/thumbnails/049/855/347/small/nature-background-high-resolution-wallpaper-for-a-serene-and-stunning-view-photo.jpg'
